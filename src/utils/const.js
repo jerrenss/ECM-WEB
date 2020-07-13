1e798a89d5bc005c369b6c9c6da44c42b2699594
jerrenss <>
2020-07-13 21:34:17 +0800

    Initial Setup

---
+++ src/utils/const.js
@@ -0,0 +1,15 @@
+export const PALETTE = {
+    text: {
+      contrast: '#FFFFFF',
+      primary: '#000000',
+    },
+    background: {
+      primary: '#EFEEEE',
+      secondary: '#D3D3D3',
+    },
+  }
+  
+  export const FONTS = {
+    montserrat: 'Montserrat',
+  }
+  
