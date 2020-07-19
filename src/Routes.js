dbcc8a28743001a56809c12fff0c0b77388ba76d
jerrenss <>
2020-07-19 15:25:32 +0800

    Enhancing products view

--- src/Routes.js
+++ src/Routes.js
@@ -10,6 +10,7 @@ import AdminDashboard from './views/user/AdminDashboard'
 import AddCategory from './views/admin/AddCategory'
 import AddProduct from './views/admin/AddProduct'
 import Shop from './views/shop'
+import Product from './views/product'
 
 const Routes = () => {
   return (
@@ -19,6 +20,7 @@ const Routes = () => {
         <Route path="/shop" exact component={Shop} />
         <Route path="/signin" exact component={SignIn} />
         <Route path="/signup" exact component={SignUp} />
+        <Route path="/product/:productId" exact component={Product} />
         <PrivateRoute path="/user/dashboard" exact component={UserDashboard} />
         <AdminRoute path="/admin/dashboard" exact component={AdminDashboard} />
         <AdminRoute path="/create/category" exact component={AddCategory} />
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,6 +3,7 @@ import makeStyles from '@material-ui/core/styles/makeStyles'
 import { Box, Typography, Button } from '@material-ui/core'
 import { Link } from 'react-router-dom'
 import ImageDisplay from './ImageDisplay'
+import moment from 'moment'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,22 +14,39 @@ const useStyles = makeStyles((theme) => ({
     border: 'solid',
     borderWidth: 'thin',
     padding: theme.spacing(2, 1),
+    '& a': {
+      textDecoration: 'none',
+      marginBottom: theme.spacing(1),
+    },
   },
 }))
 
 const ProductCard = (props) => {
   const classes = useStyles(props)
-  const { product } = props
+  const { product, hasViewProductBtn = true } = props
+
+  const showStock = () => {
+    return product.quantity > 0 ? (
+      <Typography>In Stock</Typography>
+    ) : (
+      <Typography>Out Of Stock</Typography>
+    )
+  }
 
   return (
     <Box className={classes.root}>
       <Typography>Name: {product.name}</Typography>
       <ImageDisplay item={product} url="/product" />
+      <Typography>Price: ${product.price}</Typography>
       <Typography>Description: {product.description}</Typography>
-      <Typography>Price: {product.price}</Typography>
-      <Link to="/">
-        <Button variant="outlined">View Product</Button>
-      </Link>
+      <Typography>Category: {product.category?.name}</Typography>
+      <Typography>Added: {moment(product.createdAt).fromNow()}</Typography>
+      {showStock()}
+      {hasViewProductBtn && (
+        <Link to={`/product/${product._id}`}>
+          <Button variant="outlined">View Product</Button>
+        </Link>
+      )}
       <Button variant="outlined">Add to Cart</Button>
     </Box>
   )
