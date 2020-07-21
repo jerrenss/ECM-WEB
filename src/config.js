eaa4c3a8a07a8cf8f54b83c9f477a07dc6e7aa94
jerrenss <>
2020-07-22 01:58:39 +0800

    Change API link

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // export const API = 'http://localhost:4000/api'
-export const API = 'https://blooming-dawn-01616.herokuapp.com/api'
+export const API = 'https://rocky-brook-17947.herokuapp.com/api'
